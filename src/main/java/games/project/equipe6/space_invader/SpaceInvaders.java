f01ff10f2353f6f5b28ae76aa37c76aa72b63906
ValentinG <valentin.germain01@etu.umontpellier.fr>
2023-03-12 13:19:15 +0100

    Le jeu est centré

--- src/main/java/games/project/equipe6/space_invader/SpaceInvaders.java
+++ src/main/java/games/project/equipe6/space_invader/SpaceInvaders.java
@@ -1,5 +1,7 @@
 package games.project.equipe6.space_invader;
 
+import javafx.scene.layout.Background;
+
 import java.awt.*;
 import javax.swing.*;
 
@@ -10,8 +12,19 @@ public class SpaceInvaders extends JFrame  {
     }
 
     private void initUI() {
-        add(new Board());
-
+        JPanel panel1 = new JPanel();
+        panel1.setPreferredSize(new Dimension(320, 720));
+        panel1.setBackground(Color.green);
+        Board b = new Board();
+        b.setPreferredSize(new Dimension(640, 720));
+        JPanel panel2 = new JPanel();
+        panel2.setPreferredSize(new Dimension(320, 720));
+        panel2.setBackground(Color.blue);
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(panel1, BorderLayout.WEST);
+        getContentPane().add(b, BorderLayout.CENTER);
+        getContentPane().add(panel2, BorderLayout.EAST);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Space Invaders");
         setSize(1280, 720);
 
