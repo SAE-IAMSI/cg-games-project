161db8cca178f6fa9761c1022e651d1b7f0bd897
ValentinVnh <92478691+ValentinVnh@users.noreply.github.com>
2023-03-12 21:07:37 +0100

    center + titre

--- src/main/java/games/project/space_invader/StartPanel.java
+++ src/main/java/games/project/space_invader/StartPanel.java
@@ -8,15 +8,21 @@ public class StartPanel extends JFrame {
     // TODO : Cette classe contient les éléments de la fenêtre de démarrage du jeu
     public StartPanel() {
 
+        Label title = new Label("Space Invaders");
+        title.setSize(500, 100);
+        title.setLocation(470, 10);
+        title.setFont(new Font("Arial", Font.BOLD, 50));
+        add(title);
+
         Button classic = new Button("Classic");
         classic.setSize(200, 100);
-        classic.setLocation(500, 230);
+        classic.setLocation(540, 230);
         classic.setFont(new Font("Arial", Font.BOLD, 30));
         add(classic);
 
         Button infini = new Button("Infini");
         infini.setSize(200, 100);
-        infini.setLocation(500, 330);
+        infini.setLocation(540, 340);
         infini.setFont(new Font("Arial", Font.BOLD, 30));
         add(infini);
 
@@ -26,6 +32,7 @@ public class StartPanel extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
         getContentPane().setLayout(new BorderLayout());
+        getContentPane().setBackground(Color.lightGray);
 
         classic.addActionListener(e -> {
             SpaceInvaders spaceInvaders = new SpaceInvaders("classic");
