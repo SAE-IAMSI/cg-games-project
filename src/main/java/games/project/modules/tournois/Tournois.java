f5cf9403b57b481196a79cd904bd89599790049c
auberta <alexis.aubert@etu.umontpellier.fr>
2023-04-05 17:01:52 +0200

    ajout classe tournois et TournoisTest

--- src/main/java/games/project/modules/tournois/Tournois.java
+++ src/main/java/games/project/modules/tournois/Tournois.java
@@ -1,4 +1,57 @@
 package games.project.modules.tournois;
 
+import games.project.metier.entite.Jeu;
+import games.project.metier.entite.Player;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Tournois {
+    private int IdTournoi;
+
+    private String Nomtournoi;
+
+    private Timestamp Datedebut;
+    private Timestamp Datefin;
+
+    private int NbParticipentMax;
+
+    private ArrayList<Player> ListeParticipant;
+
+    private  ArrayList<Jeu> Listedesjeux;
+
+    public Tournois(int idtournoi, String nomtournoi, Timestamp datedebut, Timestamp datefin, int nbparticipantmax, ArrayList<Jeu> listedesjeux) {
+        IdTournoi=idtournoi;
+        Nomtournoi=nomtournoi;
+        Datedebut=datedebut;
+        Datefin=datefin;
+        NbParticipentMax=nbparticipantmax;
+        ListeParticipant = new ArrayList<>();
+        Listedesjeux = listedesjeux;
+    }
+
+    public int getIdTournoi() {
+        return IdTournoi;
+    }
+
+    public String getNomtournoi(){
+        return  Nomtournoi;
+    }
+
+    public Date getDatedebut() {
+        return Datedebut;
+    }
+
+    public Date getDatefin() {
+        return Datefin;
+    }
+
+    public int getNbParticipentMax() {
+        return NbParticipentMax;
+    }
+
+    public ArrayList<Player> getListeParticipant() {
+        return ListeParticipant;
+    }
 }
--- src/test/java/games/project/modules/tournois/TournoisTest.java
+++ src/test/java/games/project/modules/tournois/TournoisTest.java
@@ -1,16 +1,32 @@
 package games.project.modules.tournois;
 
+import games.project.metier.entite.Player;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TournoisTest {
 
 
 
-   /* @Test
-    void TestCreationtournoi() {
-        Tournois t =new Tournois(String nom);
+    @Test // sert a rien
+    void TestCreationlistejoueur(String nomtournoi, int idtournoi, Timestamp datedebut, Timestamp datefin, int nbparticipantmax) {
+        Tournois t =new Tournois(idtournoi,nomtournoi,datedebut,datefin,nbparticipantmax,null);
+        assertEquals(new ArrayList<Player>(),t.getListeParticipant());
 
-    }*/
+    }
+
+
+
+    /*
+    test ajout joueur
+    test recup score par jeu
+    test  recup classement generel en fonction des position des jouer dans les jeux -> test parametrique ??
+
+
+     */
+
 }
