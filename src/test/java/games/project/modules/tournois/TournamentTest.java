6c0c51f4a09e1a96ba453992f6e10d56240ebe91
Gabin Brizay <gabin.brizay@etu.umontpellier.fr>
2023-04-05 21:02:43 +0200

    Fix TestTournoi

--- src/test/java/games/project/modules/tournois/TournamentTest.java
+++ src/test/java/games/project/modules/tournois/TournamentTest.java
@@ -1,21 +1,20 @@
 package games.project.modules.tournois;
 
 import games.project.metier.entite.Player;
+import games.project.modules.tournois.metier.entite.Tournament;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
-import static org.junit.jupiter.api.Assertions.*;
 
-class TournoisTest {
+class TournamentTest {
 
 
 
     @Test // sert a rien
     void TestCreationlistejoueur(String nomtournoi, int idtournoi, Timestamp datedebut, Timestamp datefin, int nbparticipantmax) {
-        Tournois t =new Tournois(idtournoi,nomtournoi,datedebut,datefin,nbparticipantmax,null);
-        assertEquals(new ArrayList<Player>(),t.getListeParticipant());
+        Tournament t =new Tournament(nomtournoi,datedebut,datefin,nbparticipantmax);
+        assertEquals(new ArrayList<Player>(),t.getParticipants());
 
     }
 
