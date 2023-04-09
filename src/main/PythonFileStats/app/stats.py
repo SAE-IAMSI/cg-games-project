8195f09d6abce150fb093744d333709d9ecd1e56
Skyflooze <celyan.ramirez@gmail.com>
2023-04-09 19:46:49 +0200

    Modification getNbTournois

--- src/main/PythonFileStats/app/stats.py
+++ src/main/PythonFileStats/app/stats.py
@@ -259,13 +259,13 @@ def getAvgParticipants()->float:
         for r in cursor.execute(sql):
             return r[0]
 
-def getNbTournois()->int:
+def getNbTournois()->int:   
     """
     Renvoie le nombre de tournois\n
     :return: r[0] : int
     """
     connexion = createConnexion(user, password, host, port, sid)
     with connexion.cursor() as cursor:
-        sql = """select count(*) from tournois"""
+        sql = """select getNbTournament from dual"""
         for r in cursor.execute(sql):
             return r[0]
