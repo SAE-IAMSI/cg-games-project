ddde6e292c748c141f6fe31d49065b970af47590
Gabin Brizay <gabin.brizay@etu.umontpellier.fr>
2023-04-09 19:01:49 +0200

    Ajout sécurité sur le formulaire de création.

--- src/main/java/games/project/modules/tournois/view/TournamentCreationView.java
+++ src/main/java/games/project/modules/tournois/view/TournamentCreationView.java
@@ -8,15 +8,15 @@ import games.project.modules.tournois.metier.manager.TournamentManager;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,6 +36,8 @@ public class TournamentCreationView extends AnchorPane {
     private Pane tournamentGames;
     @FXML
     private Button createTournament;
+    @FXML
+    private Label errorMessage;
 
     private List<CheckBox> gamesCheckbox;
 
@@ -73,35 +75,100 @@ public class TournamentCreationView extends AnchorPane {
 
     @FXML
     public void createNewTournament() {
-        try {
+        //Vérification qu'au moins un jeu est sélectionné.
+        if (!atLeastOneGameChecked()) {
+            errorMessage.setVisible(true);
+            errorMessage.setText("Veuillez sélectionner au minimum un jeu.");
+        } else {
+            errorMessage.setText("");
+            errorMessage.setVisible(false);
             String name = tournamentName.getText();
-            Timestamp startDate = Timestamp.valueOf(tournamentStart.getValue().atStartOfDay());
-            Timestamp endDate = Timestamp.valueOf(tournamentEnd.getValue().atStartOfDay());
-            int maxParticipants = Integer.valueOf(tournamentMax.getText());
-            List<Jeu> games = new ArrayList<>();
-            for (CheckBox cb : gamesCheckbox) {
-                if (cb.isSelected()) {
-                    String code = cb.getText().split(" - ")[0];
-                    Jeu game = JeuManager.getInstance().getJeu(code);
-                    games.add(game);
+            //Vérification qu'un nom a été saisi.
+            if (name == null || name.isEmpty()) {
+                errorMessage.setVisible(true);
+                errorMessage.setText("Veuillez saisir un nom de tournoi.");
+            } else {
+                errorMessage.setText("");
+                errorMessage.setVisible(false);
+                LocalDate start = tournamentStart.getValue();
+                LocalDate end = tournamentEnd.getValue();
+                //Vérification qu'une date de début et qu'une date de fin ont été saisies.
+                if (start == null || end == null) {
+                    errorMessage.setVisible(true);
+                    errorMessage.setText("Veuillez saisir une date de débit et une date de fin");
+                } else {
+                    errorMessage.setText("");
+                    errorMessage.setVisible(false);
+                    //Vérification que la date de début est bien antérieure à la date de fin.
+                    if (start.isAfter(end)) {
+                        errorMessage.setText("Veuillez choisir une date de début antérieure à la date de fin.");
+                        errorMessage.setVisible(true);
+                        tournamentStart.setValue(null);
+                        tournamentEnd.setValue(null);
+                    } else {
+                        Timestamp startDate = Timestamp.valueOf(start.atStartOfDay());
+                        Timestamp endDate = Timestamp.valueOf(end.atStartOfDay());
+                        String nbMax = tournamentMax.getText();
+                        //Vérification qu'un maximum a bien été saisi.
+                        if (nbMax == null || nbMax.isEmpty()) {
+                            errorMessage.setText("Veuillez saisir un entier pour Participants max");
+                            errorMessage.setVisible(true);
+                        } else {
+                            //Vérification que le maximum saisi est bien un entier.
+                            try {
+                                errorMessage.setText("");
+                                errorMessage.setVisible(false);
+                                int maxParticipants = Integer.parseInt(nbMax);
+                                //Vérification que le maximum saisi est supérieur ou égal à 2.
+                                if (maxParticipants < 2) {
+                                    errorMessage.setText("Veuillez choisir un maximum supérieur ou égal à 2.");
+                                    errorMessage.setVisible(true);
+                                } else {
+                                    errorMessage.setText("");
+                                    errorMessage.setVisible(false);
+                                    List<Jeu> games = new ArrayList<>();
+                                    for (CheckBox cb : gamesCheckbox) {
+                                        if (cb.isSelected()) {
+                                            String code = cb.getText().split(" - ")[0];
+                                            Jeu game = JeuManager.getInstance().getJeu(code);
+                                            games.add(game);
+                                        }
+                                    }
+                                    Tournament t = new Tournament(name, startDate, endDate, maxParticipants);
+                                    t.setGames(games);
+                                    TournamentManager.getInstance().createTournament(t);
+
+                                    //Remise à zéro du formulaire
+                                    tournamentName.clear();
+                                    tournamentStart.setValue(null);
+                                    tournamentEnd.setValue(null);
+                                    tournamentMax.clear();
+                                    for (CheckBox cb : gamesCheckbox) cb.setSelected(false);
+
+                                    //Retour à l'utilisateur
+                                    createTournament.setDisable(true);
+                                    createTournament.setText("Tournoi créé !");
+                                    TournamentController.delay(2000, () -> {
+                                        createTournament.setText("Créer tournoi");
+                                        createTournament.setDisable(false);
+                                    });
+                                }
+                            } catch (NumberFormatException e) {
+                                errorMessage.setText("Veuillez saisir un entier pour Participants max");
+                                errorMessage.setVisible(true);
+                                tournamentMax.clear();
+                            }
+                        }
+                    }
                 }
             }
-            Tournament t = new Tournament(name, startDate, endDate, maxParticipants);
-            t.setGames(games);
-            TournamentManager.getInstance().createTournament(t);
-            tournamentName.clear();
-            tournamentStart.setValue(null);
-            tournamentEnd.setValue(null);
-            tournamentMax.clear();
-            for (CheckBox cb : gamesCheckbox) cb.setSelected(false);
-            createTournament.setDisable(true);
-            createTournament.setText("Tournoi créé !");
-            TournamentController.delay(2000, () -> {
-                createTournament.setText("Créer tournoi");
-                createTournament.setDisable(false);
-            });
-        } catch (NullPointerException e) {
-            e.printStackTrace();
         }
+    }
+
+    private boolean atLeastOneGameChecked() {
+        for (CheckBox c : gamesCheckbox) {
+            if (c.isSelected()) return true;
+        }
+        return false;
     }
 }
