4b4240663fc60a86a7482e9375a6c5f4c7105d6f
Gabin Brizay <gabin.brizay@etu.umontpellier.fr>
2023-04-10 11:18:28 +0200

    Exemple Test avec Mockito

--- src/test/java/games/project/modules/tournois/TournamentTest.java
+++ src/test/java/games/project/modules/tournois/TournamentTest.java
@@ -5,21 +5,65 @@ import games.project.metier.entite.Jeu;
 import games.project.metier.entite.Player;
 import games.project.metier.entite.Score;
 import games.project.modules.tournois.metier.entite.Tournament;
+import games.project.modules.tournois.metier.manager.TournamentManager;
+import games.project.modules.tournois.stockage.sql.StockageTournamentDatabase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.*;
 
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class TournamentTest {
 
+    @Mock
+    private StockageTournamentDatabase stockage;
 
+    @InjectMocks
+    private TournamentManager manager;
+
+    private AutoCloseable closeable;
+
+    @BeforeEach
+    public void initMocks() {
+        closeable = MockitoAnnotations.openMocks(this);
+
+        Tournament t = new Tournament("Test1", Timestamp.valueOf(LocalDateTime.now()), Timestamp.valueOf(LocalDateTime.now().plusDays(10)), 10);
+        t.setGames(List.of(new Jeu("CB"), new Jeu("KR"), new Jeu("FF")));
+        t.setParticipants(List.of(new AuthPlayer("Test1"), new AuthPlayer("Test2"), new AuthPlayer("Test3")));
+        t.setTournamentCode(1);
+
+        Mockito.when(stockage.getAll()).thenReturn(List.of(t));
+        Mockito.when(stockage.getByCode(1)).thenReturn(t);
+        Mockito.when(stockage.getGamesByTournament(1)).thenReturn(t.getGames());
+        Mockito.when(stockage.getParticipantsByTournament(1)).thenReturn(t.getParticipants());
+        Mockito.when(stockage.getTournamentByLoginAndDate("Test1", Timestamp.valueOf(LocalDateTime.now()))).thenReturn(new ArrayList<>());
+        Mockito.when(stockage.getTournamentsByDate(Timestamp.valueOf(LocalDateTime.now()))).thenReturn(new ArrayList<>());
+        Mockito.when(stockage.getLeaderboardByGame(new Jeu("CB"), 1)).thenReturn(new ArrayList<>());
+        Mockito.when(stockage.hasPLayedOnGame("Test1", "CB", t)).thenReturn(false);
+    }
+
+    @AfterEach
+    public void releaseMocks() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    void testParticipantIn() {
+        AuthPlayer p = new AuthPlayer("Test1");
+        assertTrue(stockage.getParticipantsByTournament(1).contains(p));
+    }
 
 
     @Test
