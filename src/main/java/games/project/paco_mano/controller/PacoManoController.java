e3e4cc0fee43e63235767cc91edb93cc30fb213c
Louis Cornet <louis.cornet@umontpellier.fr>
2023-04-12 20:40:14 +0200

    ControllerFXML ajout du changement de pane

--- src/main/java/games/project/paco_mano/controller/PacoManoController.java
+++ src/main/java/games/project/paco_mano/controller/PacoManoController.java
@@ -28,7 +28,166 @@ import static javafx.scene.input.KeyCode.*;
 
 public class PacoManoController {
 
-@FXML
-private ImageView titleBackground;
+//   Panes
 
+    @FXML
+    private Pane menuPane;
+    @FXML
+    private Pane gamePane;
+    @FXML
+    private Pane playerPane;
+    @FXML
+    private Pane highscorePane;
+    @FXML
+    private Pane settingPane;
+
+
+//    Menu
+
+    @FXML
+    private ImageView titleBackground;
+    @FXML
+    private ImageView baseBackground;
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button playerButton;
+    @FXML
+    private Button highScore;
+    @FXML
+    private Button settingButton;
+    @FXML
+    private Button quitButton;
+    @FXML
+    private Button returnPlayer;
+
+//    ------------------------------------------------------------------------------------------------------------------
+
+    //    Classement
+
+    @FXML
+    private Label textTempL1;
+    @FXML
+    private Label textTempL2;
+    @FXML
+    private Label textTempL3;
+    @FXML
+    private Label textTempL4;
+    @FXML
+    private Label textTempL5;
+    @FXML
+    private Label textTempL6;
+    @FXML
+    private Label textTempL7;
+    @FXML
+    private Label textTempL8;
+    @FXML
+    private Label textTempL9;
+    @FXML
+    private Label textTempL10;
+    @FXML
+    private Label textTempS1;
+    @FXML
+    private Label textTempS2;
+    @FXML
+    private Label textTempS3;
+    @FXML
+    private Label textTempS4;
+    @FXML
+    private Label textTempS5;
+    @FXML
+    private Label textTempS6;
+    @FXML
+    private Label textTempS7;
+    @FXML
+    private Label textTempS8;
+    @FXML
+    private Label textTempS9;
+    @FXML
+    private Label textTempS10;
+    @FXML
+    private Label textTempD1;
+    @FXML
+    private Label textTempD2;
+    @FXML
+    private Label textTempD3;
+    @FXML
+    private Label textTempD4;
+    @FXML
+    private Label textTempD5;
+    @FXML
+    private Label textTempD6;
+    @FXML
+    private Label textTempD7;
+    @FXML
+    private Label textTempD8;
+    @FXML
+    private Label textTempD9;
+    @FXML
+    private Label textTempD10;
+    @FXML
+    private Button returnHighscore;
+
+//    ------------------------------------------------------------------------------------------------------------------
+
+//   Settings
+
+    @FXML
+    private Button returnSetting;
+    @FXML
+    private Button upButton;
+    @FXML
+    private Button leftButton;
+    @FXML
+    private Button downButton;
+    @FXML
+    private Button rightButton;
+
+//    ------------------------------------------------------------------------------------------------------------------
+
+//   Pane change functions
+
+    /** To menu */
+    @FXML
+    protected void backToMenuFromSettings() {
+        settingPane.setVisible(false);
+        menuPane.setVisible(true);
+    }
+    @FXML
+    protected void backToMenuFromPlayerPane() {
+        playerPane.setVisible(false);
+        menuPane.setVisible(true);
+    }
+    @FXML
+    protected void backToMenuFromHighScore() {
+        highscorePane.setVisible(false);
+        menuPane.setVisible(true);
+    }
+    @FXML
+    protected void backToMenuFromGame() {
+        gamePane.setVisible(false);
+        menuPane.setVisible(true);
+    }
+
+    /** Menu to */
+    @FXML
+    protected void menuToSettings() {
+        settingPane.setVisible(true);
+        menuPane.setVisible(false);
+    }
+    @FXML
+    protected void menuToPlayerPane() {
+        playerPane.setVisible(true);
+        menuPane.setVisible(false);
+    }
+    @FXML
+    protected void menuToHighScore() {
+        highscorePane.setVisible(true);
+        menuPane.setVisible(false);
+    }
+    @FXML
+    protected void menuToGame() {
+        gamePane.setVisible(true);
+        menuPane.setVisible(false);
+    }
 }
